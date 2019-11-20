7a000e5c6a3d3576ad38dc761b2a866760588e4c
isa-ardi <54489663+isa-ardi@users.noreply.github.com>
2019-11-20 18:38:26 +0700

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Parason Software - Home</title>
-	<link rel="icon" href="img/Fevicon.png" type="image/png">
+  <title></title>
+	<link rel="icon" href="img/home/logo.png" type="image/png">
 
   <link rel="stylesheet" href="vendors/bootstrap/bootstrap.min.css">
   <link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
@@ -15,15 +15,22 @@
   <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
 
   <link rel="stylesheet" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <style>
+  body {
+    position: relative; 
+  }
+  </style>
 </head>
 <body>
   <!--================Header Menu Area =================-->
   <header class="header_area">
     <div class="main_menu">
-      <nav class="navbar navbar-expand-lg navbar-light" style="background: black;">
+      <nav class="navbar navbar-expand-lg navbar-light"id="myNavbar" style="background: black;">
         <div class="container box_1620">
           <!-- Brand and toggle get grouped for better mobile display -->
-          <a class="navbar-brand logo_h" href="index.php"><img src="img/logo.png" height="50%" width="50%"></a>
+          <a class="navbar-brand logo_h" href="index.php"><img src="img/logo.png" height="%" width="60%"></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -32,10 +39,10 @@
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
             <ul class="nav navbar-nav menu_nav justify-content-end">
-              <li class="nav-item active"><a class="nav-link" href="index.php">Home</a></li> 
-              <li class="nav-item"><a class="nav-link" href="feature.php">Feature</a></li> 
-              <li class="nav-item"><a class="nav-link" href="pricing.php">Price</a>
-              <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+              <li class="nav-item active"><a class="nav-link" href="#section1">Home</a></li> 
+              <li class="nav-item"><a class="nav-link" href="#section2">Feature</a></li> 
+              <li class="nav-item"><a class="nav-link" href="#section3">Selection</a>
+              <li class="nav-item"><a class="nav-link" href="#section4">About</a></li>
             </ul>
             </ul>
           </div> 
