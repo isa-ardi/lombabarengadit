eb6a716758f907fa700b97a55fb26a0632541e2b
isa-ardi <54489663+isa-ardi@users.noreply.github.com>
2019-11-20 18:37:41 +0700

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,76 +1,52 @@
-
   <!-- ================ start footer Area ================= -->
   <footer class="footer-area section-gap" style="background: black;">
-		<div class="container">
+		<div class="container"id="section4">
 			<div class="row">
-				<div class="col-xl-2 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
-					<h4>Top Products</h4>
-					<ul>
-						<li><a href="#">Managed Website</a></li>
-						<li><a href="#">Manage Reputation</a></li>
-						<li><a href="#">Power Tools</a></li>
-						<li><a href="#">Marketing Service</a></li>
-					</ul>
-				</div>
-				<div class="col-xl-2 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
-					<h4>Quick Links</h4>
-					<ul>
-						<li><a href="#">Jobs</a></li>
-						<li><a href="#">Brand Assets</a></li>
-						<li><a href="#">Investor Relations</a></li>
-						<li><a href="#">Terms of Service</a></li>
-					</ul>
-				</div>
-				<div class="col-xl-2 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
-					<h4>Features</h4>
-					<ul>
-						<li><a href="#">Jobs</a></li>
-						<li><a href="#">Brand Assets</a></li>
-						<li><a href="#">Investor Relations</a></li>
-						<li><a href="#">Terms of Service</a></li>
-					</ul>
-				</div>
-				<div class="col-xl-2 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
-					<h4>Resources</h4>
-					<ul>
-						<li><a href="#">Guides</a></li>
-						<li><a href="#">Research</a></li>
-						<li><a href="#">Experts</a></li>
-						<li><a href="#">Agencies</a></li>
-					</ul>
-				</div>
-				<div class="col-xl-4 col-md-8 mb-4 mb-xl-0 single-footer-widget">
-					<h4>Newsletter</h4>
-					<p>You can trust us. we only send promo offers,</p>
-					<div class="form-wrap" id="mc_embed_signup">
-						<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
-						 method="get" class="form-inline">
-							<input class="form-control" name="EMAIL" placeholder="Your Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Email Address '"
-							 required="" type="email">
-							<button class="click-btn btn btn-default">subscribe</button>
-							<div style="position: absolute; left: -5000px;">
-								<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
-							</div>
+			<div class="col-xl-1 col-sm-6 mb-4 mb-xl-0 single-footer-widget"></div>
 
-							<div class="info"></div>
-						</form>
-					</div>
+			
+				<div class="col-xl-2 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+					<h4>Ikuti Kami</h4>
+					<ul>
+						<li><a href="#"><i class="fab fa-instagram fa-lg"></i>&nbsp;Instagram</a></li>
+						<li><a href="#"><i class="fab fa-facebook fa-lg"></i>&nbsp;Facebook</a></li>
+						<li><a href="#"><i class="fas fa-envelope fa-lg"></i>&nbsp;Email</a></li>
+						<li><a href="#"><i class="fab fa-youtube fa-lg"></i>&nbsp;YouTube</a></li>
+					</ul>
 				</div>
+		
+				<div class="col-xl-4 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+					<h4>Detail Sekolah:</h4>
+					<ul>
+						<li><a href="#">Alamat  : Jl. Stadion Barat, Sisir, Kec. Batu, Kota Batu</a></li>
+						<li><a href="#">Kode Pos: 65314</a></li>
+						<li><a href="#">Provinsi: Jawa Timur</a></li>
+						<li><a href="#">No Telp : (0341) 597079</a></li>
+					</ul>
+				</div>
+				
+				
+				<div class="col-xl-4 col-md-8 mb-4 mb-xl-0 single-footer-widget">
+
+				
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7904.32280717676!2d112.525688!3d-7.878175000000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1506c8a5f59d050f!2sSMK%20Islam%20Kota%20Batu!5e0!3m2!1sen!2sid!4v1567753156743!5m2!1sen!2sid" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 			</div>
 			<div class="footer-bottom row align-items-center text-center text-lg-left">
 				<p class="footer-text m-0 col-lg-8 col-md-12"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());
+// Material Select Initialization
+$(document).ready(function() {
+$('.mdb-select').materialSelect();
+});
+</script> 
+All rights reserved   <i class="fa fa-heart" aria-hidden="true">by <a href="https://adityasundawa.com" target="_blank">Smk Islam Batu</a></i> 
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-				<div class="col-lg-4 col-md-12 text-center text-lg-right footer-social">
-					<a href="#"><i class="fab fa-facebook-f"></i></a>
-					<a href="#"><i class="fab fa-twitter"></i></a>
-					<a href="#"><i class="fab fa-dribbble"></i></a>
-					<a href="#"><i class="fab fa-behance"></i></a>
-				</div>
 			</div>
 		</div>
 	</footer>
   <!-- ================ End footer Area ================= -->
+
+  
 
   <script src="vendors/jquery/jquery-3.2.1.min.js"></script>
   <script src="vendors/bootstrap/bootstrap.bundle.min.js"></script>
@@ -78,5 +54,5 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
   <script src="js/jquery.ajaxchimp.min.js"></script>
   <script src="js/mail-script.js"></script>
   <script src="js/main.js"></script>
-</body>
+  </body>
 </html>
